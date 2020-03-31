2ca724f26f1060af7e459ef6d03332fcb22c2c78
Meesam Haider <meesam.haider@vividseats.com>
2020-03-31 12:16:36 -0400

    Updated for heroku deployment

--- src/api/http.js
+++ src/api/http.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-	baseURL: process.env.NODE_ENV === "production" ? "//someproductionurl.com" : "//localhost:8000"
+	baseURL: process.env.NODE_ENV === "production" ? "//get-bread-api-node.herokuapp.com" : "//localhost:8000"
 });
 
 export default instance
